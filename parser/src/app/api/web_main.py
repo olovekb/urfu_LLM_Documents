92f72524eea8b7e5f82ab3791373daee5172bc79
UED17_OlegAnkudinov <olegsip2002@gmail.com>
2025-05-10 16:26:54 +0500

    Убрал файлы по второй проверке и старый лог

--- parser/src/app/api/web_main.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-pg = st.navigation([st.Page("web_gui.py",title="Конвертировать справочники"),
-                    st.Page("web_update1.py", title="Получить шаблон справочника")])
-pg.run()
--- parser/src/app/test/test_get_doc.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-sys.path.append("/media/kirilman/Z2/Project/Sber_document/documents/src/")
-sys.path.append("./")
-print(sys.path)
-from src.app.checker.utils.text_utils import read_text_documents
-p = "src/app/data/test/Миасс,_геологоразведочный_колледж,_оп_1_пост.pdf"
-text, full_text, page_blocks = read_text_documents(p)
-print(page_blocks)
