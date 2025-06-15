373b394d7e1493a83e78a6bff18c1870618f0ce3
UED17_OlegAnkudinov <olegsip2002@gmail.com>
2025-06-16 00:51:16 +0500

    Текст из выделенных  блоков выводится в консоль, однако выдяляются лишние блоки на страницах

--- lama 3.1/proverka8/DocumentChecker.py
+++ lama 3.1/proverka8/DocumentChecker.py
@@ -1,36 +1,30 @@
 import cv2
 import pytesseract
 import os
-from cv2.gapi import kernel
+from pathlib import Path
 from pdf2image import convert_from_path
 from sklearn.cluster import DBSCAN
-from pathlib import Path
 
 def local_to_absolute_path(file_path):
     return str(Path(file_path).resolve())
 
-"""
-# Загрузка модели эмбеддингов
-model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+# Очищаем папку от старых файлов
+def clear_folder(folder_path):
+    # Проверка, существует ли папка
+    if not os.path.exists(folder_path):
+        print(f"Папка не найдена: {folder_path}")
+        return
+    
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        try:
+            if os.path.isfile(file_path):
+                os.remove(file_path)  # Удаление файла
+                print(f"Удален файл: {file_path}")
+        except Exception as e:
+            print(f'Ошибка при удалении файла {file_path}: {e}')
 
-# Сравнение с использованием эмбеддингов напрямую
-def compare_names_with_embeddings(name1, name2, threshold=0.8):
-    if name1 and name2:
-        name1_clean = preprocess_text(name1)
-        name2_clean = preprocess_text(name2)
-        emb1 = model.encode(name1_clean, convert_to_tensor=True)
-        emb2 = model.encode(name2_clean, convert_to_tensor=True)
-        similarity = util.pytorch_cos_sim(emb1, emb2).item()
-
-        if similarity >= threshold:
-            return f"Наименования совпадают (сходство: {similarity:.2f})"
-        else:
-            return f"Наименования не совпадают (сходство: {similarity:.2f})"
-    else:
-        return "Ошибка: Не удалось извлечь наименования"
-"""
-
-#Преобразование pdf к jpg
+# Преобразование pdf к jpg
 def convertPDFToImage(file_path, save_image_path = local_to_absolute_path('lama 3.1/proverka8/data/pdf_images'), poppler_path = local_to_absolute_path('urfu_LLM_Documents/lama 3.1/poppler-24.08.0/Library/bin')):
     deleteFileInFolder()
     os.environ["PATH"] += os.pathsep + poppler_path
@@ -39,7 +33,7 @@ def convertPDFToImage(file_path, save_image_path = local_to_absolute_path('lama 
     for i, image in enumerate(images, start=1):
         image.save(f'{save_image_path}\\page_{i}.jpg', 'JPEG')
 
-#Удаление предыдущих результатов преобразования pdf к jpg
+# Удаление предыдущих результатов преобразования pdf к jpg
 def deleteFileInFolder(file_path = local_to_absolute_path('lama 3.1/proverka8/data/pdf_images')):
     for filename in os.listdir(file_path):
         file_path = os.path.join(file_path, filename)
@@ -49,20 +43,20 @@ def deleteFileInFolder(file_path = local_to_absolute_path('lama 3.1/proverka8/da
         except Exception as e:
             print(f'Ошибка при удалении файла {file_path}. {e}')
 
-#Анализ страницы
+# Анализ страницы
 def analyzeImages(images_path = local_to_absolute_path('lama 3.1/proverka8/data/pdf_images')):
     pytesseract.pytesseract.tesseract_cmd = local_to_absolute_path('lama 3.1/proverka8/Tesseract/tesseract.exe')
 
-    #Открываем контекст одной страницы
+    # Открываем контекст одной страницы
     for i in range(1, len(os.listdir(images_path)) + 1):
-        img = cv2.imread(f'{local_to_absolute_path('lama 3.1/proverka8/data/pdf_images')}\\page_{i}.jpg')
+        img = cv2.imread(f'{local_to_absolute_path("lama 3.1/proverka8/data/pdf_images")}\\page_{i}.jpg')
 
         data = pytesseract.image_to_data(img, lang='rus', output_type=pytesseract.Output.DICT)
 
         boxes = []
         centers = []
 
-        #0 - утверждено, 1 - согласовано
+        # 0 - утверждено, 1 - согласовано
         flag_arr = [False, False]
         left_arr = [0, 0]
         top_arr = [0, 0]
@@ -70,6 +64,7 @@ def analyzeImages(images_path = local_to_absolute_path('lama 3.1/proverka8/data/
         # Сбор прямоугольников для текста
         for iterator in range(len(data['text'])):
 
+            # Условие для поиска нужных наименований
             if 'утверждаю' in data['text'][iterator].lower().strip() or 'утверждено' in data['text'][iterator].lower().strip():
                 flag_arr[0] = True
                 left_arr[0] = int(data['left'][iterator])
@@ -80,6 +75,7 @@ def analyzeImages(images_path = local_to_absolute_path('lama 3.1/proverka8/data/
                 left_arr[1] = int(data['left'][iterator])
                 top_arr[1] = int(data['top'][iterator])
 
+            # Сбор прямоугольников для блоков с наименованиями
             if flag_arr[0]:
                 if int(data['conf'][iterator]) > 50 and flag_arr[0] and data['text'][iterator] != None and (left_arr[0] - 250 <= data['left'][iterator] or left_arr[0] + 250 >= data['left'][iterator]) and (top_arr[0] - 500 <= data['top'][iterator] and top_arr[0] + 500 >= data['top'][iterator]):
                     x, y, w, h = data['left'][iterator], data['top'][iterator], data['width'][iterator], data['height'][iterator]
@@ -119,36 +115,32 @@ def analyzeImages(images_path = local_to_absolute_path('lama 3.1/proverka8/data/
             x_min, y_min = min(xs), min(ys)
             x_max, y_max = max(ws), max(hs)
 
+            # Рисуем прямоугольники вокруг блоков текста
             cv2.rectangle(img, (x_min, y_min), (x_max, y_max), (10, 255, 12), 2)
 
-        cv2.imwrite(f'{local_to_absolute_path('lama 3.1/proverka8/data/change_image')}\\page_{i}.jpg', img)
+            # Извлечение текста внутри рамки
+            cropped_img = img[y_min:y_max, x_min:x_max]
+            text = pytesseract.image_to_string(cropped_img, lang='rus')
 
-        """
-        #Выделение блоков на изображении
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(gray, (7, 7), 0)
-        thresh = cv2.threshold(blur, 0, 255,cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-        kernal = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 13))
-        dilate = cv2.dilate(thresh, kernal, iterations=1)
+            if text.strip():  # Если текст не пустой, выводим его
+                print(f"Текст внутри рамки на странице {i}:")
+                print(text)
+                print("\n")
 
-
-        cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-        cnts = sorted(cnts, key=lambda x: cv2.boundingRect(x)[0])
-        for c in cnts:
-            (x, y, w, h) = cv2.boundingRect(c)
-            #if h > 100 and w > 200:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (36, 255, 12), 2)
-        cv2.imwrite(f'G:\\Python\\urfu_LLM_documents\\lama 3.1\\proverka8\\data\\change_image\\page_{i}.jpg', img)
-        """
+        # Сохраняем изображение с выделенными блоками
+        cv2.imwrite(f'{local_to_absolute_path("lama 3.1/proverka8/data/change_image")}\\page_{i}.jpg', img)
 
 # Основная логика
 def main(pdf_path):
+    # Очищаем папки pdf_images и change_image перед запуском
+    clear_folder(local_to_absolute_path('lama 3.1/proverka8/data/pdf_images'))
+    clear_folder(local_to_absolute_path('lama 3.1/proverka8/data/change_image'))
+
     convertPDFToImage(pdf_path)
     analyzeImages()
     return f"Images successfully converted from {pdf_path}"
 
 # Запуск
-pdf_path = r'G:/Python/urfu_LLM_documents/lama 3.1/proverka8/data/СП_16_Магнит_отдел_Выборы_Гос_Дума_2016_оп_2_п_хр_.pdf'
+pdf_path = r'G:/Python/urfu_LLM_documents/lama 3.1/proverka8/data/Еткуль, УСЗН, оп 1, п.хр.2021.pdf'
 
 main(pdf_path)
