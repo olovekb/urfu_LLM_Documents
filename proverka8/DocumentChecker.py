4840e8219bfa8927c64aa457d92d23d4502c2fbe
UED17_OlegAnkudinov <olegsip2002@gmail.com>
2025-06-22 17:58:05 +0500

    Новая версия кода

--- proverka8/DocumentChecker.py
+++ proverka8/DocumentChecker.py
@@ -1,15 +1,54 @@
 import cv2
 import numpy as np
 import os
+import pytesseract
+import re
 from cv2.gapi import kernel
 from pdf2image import convert_from_path
 from pathlib import Path
 
 def local_to_absolute_path(file_path):
-    return str(Path(file_path).resolve())
+    base_path = r'G:\Python\urfu_LLM_documents\proverka8'
+    return str(Path(base_path, file_path).resolve())
+
+
+def remove_letters(image_path= local_to_absolute_path('data/pdf_images')):
+    """
+    Удаляет все буквы из текста на изображении JPG, оставляя цифры и символы.
+
+    Аргументы:
+        image_path (str): Путь к входному изображению в формате JPG.
+
+    Результат:
+        Сохраняет новое изображение как 'output.jpg'.
+    """
+    pytesseract.pytesseract.tesseract_cmd = r'G:\Python\urfu_LLM_documents\Libraries\Tesseract\tesseract.exe'
+
+    for iterator in range(1, len(os.listdir(image_path)) + 1):
+
+        image = cv2.imread(image_path + f'/page_{iterator}.jpg')
+
+        if image is None:
+            print("Ошибка: Не удалось загрузить изображение.")
+            return
+
+        # Получение данных OCR
+        data = pytesseract.image_to_data(image, lang='rus', output_type=pytesseract.Output.DICT)
+        month_array = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'ноябрь', 'декабрь']
+
+        # Поиск букв и закрашивание
+        for i in range(len(data['text'])):
+            text = data['text'][i].strip()
+            if text and re.match(r'^[а-яА-Я,!@#$%^&*().?<>№]', text) and text not in month_array:  # Проверка, является ли слово только буквами
+                x, y, w, h = data['left'][i], data['top'][i], data['width'][i], data['height'][i]
+                cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 255), -1)  # Закрашивание белым
+
+        # Сохранение нового изображения
+        cv2.imwrite(local_to_absolute_path(f'data/pdf_images_without_letters/page_{iterator}.jpg'), image)
+        print("Изображение сохранено")
 
 # Преобразование pdf к jpg
-def convert_PDF_to_image(file_path, save_image_path = local_to_absolute_path('proverka8/data/pdf_images'), poppler_path = local_to_absolute_path('urfu_LLM_documents/Libraries/poppler-24.08.0/Library/bi')): 
+def convert_PDF_to_image(file_path, save_image_path = local_to_absolute_path('data/pdf_images'), poppler_path = local_to_absolute_path('urfu_LLM_documents/Libraries/poppler-24.08.0/Library/bin')):
     print('Шаг 1 Конвертация pdf к изображению...')
     os.environ["PATH"] += os.pathsep + poppler_path
     images = convert_from_path(file_path)
@@ -18,10 +57,10 @@ def convert_PDF_to_image(file_path, save_image_path = local_to_absolute_path('pr
         image.save(f'{save_image_path}\\page_{i}.jpg', 'JPEG')
 
 # Построение таблицы
-def build_table(images_path = local_to_absolute_path('proverka8/data/pdf_images')):
+def build_table(images_path = local_to_absolute_path('data/pdf_images_without_letters')):
 
     for iterator in range(1, len(os.listdir(images_path)) + 1):
-        image_path = local_to_absolute_path('proverka8/data/pdf_images') + f'\\page_{iterator}.jpg'
+        image_path = local_to_absolute_path('data/pdf_images_without_letters') + f'\\page_{iterator}.jpg'
         image = cv2.imread(image_path)
         
         # Проверка, что изображение успешно загружено
@@ -69,20 +108,20 @@ def build_table(images_path = local_to_absolute_path('proverka8/data/pdf_images'
             for y in range(top, bottom, row_height):
                 cv2.line(image, (left, y), (right, y), (0, 0, 0), 2)
 
-            cv2.imwrite(local_to_absolute_path('proverka8/change_image') + f'\\result_image_{iterator}.jpg', image)
+            cv2.imwrite(local_to_absolute_path('change_image') + f'\\result_image_{iterator}.jpg', image)
 
 # Удаление файлов из всех папок
 def delete_file_in_folder():
     folders_to_delete = [
-        r'G:\Python\urfu_LLM_documents\proverka8\change_image',
-        r'G:\Python\urfu_LLM_documents\proverka8\gray_image',
-        r'G:\Python\urfu_LLM_documents\proverka8\blur_image',
-        r'G:\Python\urfu_LLM_documents\proverka8\threshold_image',
-        r'G:\Python\urfu_LLM_documents\proverka8\kernal_image',
-        r'G:\Python\urfu_LLM_documents\proverka8\dilate_image',
-        r'G:\Python\urfu_LLM_documents\proverka8\bbox_image',
-        r'G:\Python\urfu_LLM_documents\proverka8\cropped_image'
-
+        local_to_absolute_path('change_image'),
+        local_to_absolute_path('gray_image'),
+        local_to_absolute_path('blur_image'),
+        local_to_absolute_path('threshold_image'),
+        local_to_absolute_path('kernal_image'),
+        local_to_absolute_path('dilate_image'),
+        local_to_absolute_path('bbox_image'),
+        local_to_absolute_path('cropped_image'),
+        local_to_absolute_path('data/pdf_images_without_letters'),
     ]
 
     for folder in folders_to_delete:
@@ -102,8 +141,8 @@ def delete_file_in_folder():
 
 # Преобразование всех изображений в серые
 def convert_to_gray():
-    image_folder = local_to_absolute_path(r'G:\Python\urfu_LLM_documents\proverka8\change_image')
-    gray_folder = local_to_absolute_path(r'G:\Python\urfu_LLM_documents\proverka8\gray_image')
+    image_folder = local_to_absolute_path(r'change_image')
+    gray_folder = local_to_absolute_path(r'gray_image')
     
     # Проходим по изображениям в папке change_image и конвертируем их в серые
     for filename in os.listdir(image_folder):
@@ -119,15 +158,15 @@ def convert_to_gray():
 
 # Применение размытия к изображениям
 def apply_gaussian_blur():
-    gray_folder = local_to_absolute_path(r'G:\Python\urfu_LLM_documents\proverka8\gray_image')
-    blur_folder = local_to_absolute_path(r'G:\Python\urfu_LLM_documents\proverka8\blur_image')
+    gray_folder = local_to_absolute_path(r'gray_image')
+    blur_folder = local_to_absolute_path(r'blur_image')
     
     # Проходим по изображениям в папке gray_image и применяем размытие
     for filename in os.listdir(gray_folder):
         if filename.endswith(".jpg"):
             img_path = os.path.join(gray_folder, filename)
             image = cv2.imread(img_path)
-            blurred_image = cv2.GaussianBlur(image, (7, 7), 0)  # Применение размытия
+            blurred_image = cv2.GaussianBlur(image, (9, 9), 0)  # Применение размытия
             
             # Сохраняем результат в папке blur_image
             blur_path = os.path.join(blur_folder, filename)
@@ -136,8 +175,8 @@ def apply_gaussian_blur():
 
 # Применение пороговой обработки
 def apply_threshold():
-    blur_folder = local_to_absolute_path(r'G:\Python\urfu_LLM_documents\proverka8\blur_image')
-    threshold_folder = local_to_absolute_path(r'G:\Python\urfu_LLM_documents\proverka8\threshold_image')
+    blur_folder = local_to_absolute_path(r'blur_image')
+    threshold_folder = local_to_absolute_path(r'threshold_image')
     
     # Проходим по изображениям в папке blur_image и применяем пороговую обработку
     for filename in os.listdir(blur_folder):
@@ -159,8 +198,8 @@ def apply_threshold():
 
 # Применение техники морфологического преобразования (kernal)
 def apply_kernal():
-    threshold_folder = local_to_absolute_path(r'G:\Python\urfu_LLM_documents\proverka8\threshold_image')
-    kernal_folder = local_to_absolute_path(r'G:\Python\urfu_LLM_documents\proverka8\kernal_image')
+    threshold_folder = local_to_absolute_path(r'threshold_image')
+    kernal_folder = local_to_absolute_path(r'kernal_image')
     
     # Создаем ядро для морфологической операции
     kernal = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 13))
@@ -178,11 +217,11 @@ def apply_kernal():
 
 # Применение техники dilate
 def apply_dilate():
-    threshold_folder = local_to_absolute_path(r'G:\Python\urfu_LLM_documents\proverka8\threshold_image')
-    dilate_folder = local_to_absolute_path(r'G:\Python\urfu_LLM_documents\proverka8\dilate_image')
+    threshold_folder = local_to_absolute_path(r'threshold_image')
+    dilate_folder = local_to_absolute_path(r'dilate_image')
     
     # Создаем ядро для dilate
-    kernal = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 27))
+    kernal = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 30))
     
     # Проходим по изображениям в папке threshold_image и применяем dilate
     for filename in os.listdir(threshold_folder):
@@ -198,12 +237,11 @@ def apply_dilate():
             cv2.imwrite(dilate_path, dilate)
             print(f"Сохранено изображение с dilate: {dilate_path}")
 
-def build_table_after_preprocessing(images_path = local_to_absolute_path('proverka8/dilate_image')):
-    bbox_image_folder = r'G:\Python\urfu_LLM_documents\proverka8\bbox_image'
-    change_image_folder = r'G:\Python\urfu_LLM_documents\proverka8\change_image'  
-    сropped_image_folder = r'G:\Python\urfu_LLM_documents\proverka8\cropped_image'  
+def build_table_after_preprocessing(images_path = local_to_absolute_path('dilate_image')):
+    bbox_image_folder = local_to_absolute_path('bbox_image')
+    change_image_folder = local_to_absolute_path('change_image')  # Папка с исходными изображениями
 
-
+    count = 0
     for iterator in range(1, len(os.listdir(images_path)) + 1):
         dilate_path = os.path.join(images_path, f'result_image_{iterator}.jpg')
         dilate = cv2.imread(dilate_path)
@@ -237,18 +275,18 @@ def build_table_after_preprocessing(images_path = local_to_absolute_path('prover
         # Сортировка контуров по координате X (влево-направо)
         cnts = sorted(cnts, key=lambda x: cv2.boundingRect(x)[0])
 
-        # Рисование прямоугольников для каждого контура на исходном изображении
-        cropped_image_folder = local_to_absolute_path(r'G:\Python\urfu_LLM_documents\proverka8\cropped_image')
+        cropped_image_folder = local_to_absolute_path(r'cropped_image')
 
+        # Рисование прямоугольников для каждого контура на исходном изображении
         for c in cnts:
-            x, y, w, h = cv2.boundingRect(c)
-            if w > 150 and h > 250:  # Фильтрация по размерам столбцов
-                roi = image[y:y+h, x:x+h]
-                cropped_image_path = os.path.join(cropped_image_folder, f"cropped_image_{iterator}.jpg")
+            x, y, w, h = cv2.boundingRect(c) 
+            if w > 120 and h > 40 and w < 1500 and h < 3000:  # Фильтрация по размерам столбцов
+                count += 1
+                roi = image[y:y + h, x:x + h]
+                cropped_image_path = os.path.join(cropped_image_folder, f"cropped_image_{count}.jpg")
                 cv2.imwrite(cropped_image_path, roi)
                 print(f"Сохранено обрезанное изображение: {cropped_image_path}")
-                cv2.rectangle(image, (x, y), (x + w, y + h), (36, 255, 12), 2)
-            
+
         # Сохранение изображения с прямоугольниками
         result_image_path = os.path.join(bbox_image_folder, f'result_image_{iterator}.jpg')
         cv2.imwrite(result_image_path, image)
@@ -257,8 +295,9 @@ def build_table_after_preprocessing(images_path = local_to_absolute_path('prover
 
 def main():
     delete_file_in_folder()  # Удаляем файлы из всех папок
-    pdf_path = r'G:\Python\urfu_LLM_documents\proverka8\data\pdf_images\Кунашак, КРК, оп 1, п.хр. (3).pdf'  # Укажите путь к своему PDF
+    pdf_path = r'G:\Python\urfu_LLM_documents\proverka8\data\МАГНИТ_МКУ,_ЗАО_БЕЛКА,_Ф_Р_507,_ОП_697,ЛС_1.pdf'  
     convert_PDF_to_image(pdf_path)
+    remove_letters()
     build_table()
     convert_to_gray()  # Преобразуем все изображения в серые
     apply_gaussian_blur()  # Применяем размытие к изображениям
